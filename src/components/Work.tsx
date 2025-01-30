10d3fd6cbd518373fd1598aa3d75ae3bddb52764
Zodiax <zodiaxcore@gmail.com>
2025-01-30 17:27:02 +0500

    Update Work.tsx

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -41,6 +41,26 @@ const Work = () => {
       delay: 0.2,
     });
   }, []);
+
+    const workItems = [
+    { title: "Aura By Apple", category: "Design", tools: "Figma" },
+    { title: "Spotify", category: "Design , UI/UX", tools: "Figma" },
+    { title: "Crtical Thinking", category: "Design", tools: "Adobe Illustrator" },
+    { title: "Ace", category: "Design", tools: "Figma" },
+    { title: "ZYNO", category: "Web Devolopment", tools: "react , Vue , Tailwind , Node" },
+    { title: "Amazon Prime", category: "Redesign", tools: "Adobe Illustrator, Photoshop" },
+
+  ];
+
+  const workImages = [
+    "/images/work01.jpeg",
+    "/images/work02.jpeg",
+    "/images/work03.jpeg",
+    "/images/work04.jpeg",
+    "/images/work05.jpeg",
+    "/images/work06.jpeg",
+  ];
+  
   return (
     <div className="work-section" id="work">
       <div className="work-container section-container">
@@ -48,21 +68,21 @@ const Work = () => {
           My <span>Work</span>
         </h2>
         <div className="work-flex">
-          {[...Array(6)].map((_value, index) => (
+          {workItems.slice(0, 6).map((item, index) => (
             <div className="work-box" key={index}>
               <div className="work-info">
                 <div className="work-title">
                   <h3>0{index + 1}</h3>
 
                   <div>
-                    <h4>Project Name</h4>
-                    <p>Category</p>
+                    <h4 tabIndex={1}>{item.title}</h4>
+                    <p tabIndex={1}>{item.category}</p>
                   </div>
                 </div>
                 <h4>Tools and features</h4>
-                <p>Javascript, TypeScript, React, Threejs</p>
+                <p>{item.tools}</p>
               </div>
-              <WorkImage image="/images/placeholder.webp" alt="" />
+              <WorkImage image={workImages[index]} alt={item.title} />
             </div>
           ))}
         </div>
