aa5e826f34ae1558ea89544b8f37e1ddeaf270bb
Zodiax <zodiaxcore@gmail.com>
2025-01-30 17:22:21 +0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,14 +43,14 @@ const Navbar = () => {
     <>
       <div className="header">
         <a href="/#" className="navbar-title" data-cursor="disable">
-          Logo
+          z.
         </a>
         <a
           href="mailto:example@mail.com"
           className="navbar-connect"
           data-cursor="disable"
         >
-          example@mail.com
+          zodiaxcore@gmail.com
         </a>
         <ul>
           <li>
