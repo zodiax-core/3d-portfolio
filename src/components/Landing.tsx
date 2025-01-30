0b37c18ada79c0abdad43e5a0912cdc193e70114
Zodiax <zodiaxcore@gmail.com>
2025-01-30 17:21:31 +0500

    Update Landing.tsx

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -7,22 +7,22 @@ const Landing = ({ children }: PropsWithChildren) => {
       <div className="landing-section" id="landingDiv">
         <div className="landing-container">
           <div className="landing-intro">
-            <h2>Hello! I'm</h2>
+            <h2>Welcome</h2>
             <h1>
-              MONCY
+              to 
               <br />
-              <span>YOHANNAN</span>
+              <span>Zodiax</span>
             </h1>
           </div>
           <div className="landing-info">
-            <h3>A Creative</h3>
+            <h3>Agency of Creative</h3>
             <h2 className="landing-info-h2">
-              <div className="landing-h2-1">Designer</div>
-              <div className="landing-h2-2">Developer</div>
+              <div className="landing-h2-1">Designers</div>
+              <div className="landing-h2-2">Developers</div>
             </h2>
             <h2>
-              <div className="landing-h2-info">Developer</div>
-              <div className="landing-h2-info-1">Designer</div>
+              <div className="landing-h2-info">Developers</div>
+              <div className="landing-h2-info-1">Designers</div>
             </h2>
           </div>
         </div>
